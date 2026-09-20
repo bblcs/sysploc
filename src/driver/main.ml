e0f5266884dd4c2ca45a7042819b9011a4e78a29
evlc <dodgedodgedoggy@gmail.com>
2026-09-20 22:23:25 +0700

    clean up the driver

--- src/driver/main.ml
+++ src/driver/main.ml
@@ -1,47 +1,46 @@
 open Frontend
 
-let there_were_errors = ref false
-
 let print_error e =
-  Format.printf "Error at %d:%d: %s\n" (e.Parser.pos.line + 1) (e.pos.col + 1)
+  Format.eprintf "Error at %d:%d: %s\n" (e.Parser.pos.line + 1) (e.pos.col + 1)
     e.msg
 
-let rec extract_lex_errors tokens =
-  List.filter_map
-    (fun t ->
-      match t.Token.kind with
-      | Token.Err msg -> Some Parser.{ pos = t.pos; msg }
-      | Token.ErrInvalidChar c ->
-          Some
-            Parser.
-              { pos = t.pos; msg = Format.sprintf "Invalid character '%c'" c }
-      | _ -> None)
-    tokens
-
-let rec extract_lex_nonerrors tokens =
-  List.filter (fun t -> not (Token.is_error t)) tokens
+let rec separate_tokens tokens =
+  let rec loop valid errs = function
+    | [] -> (List.rev valid, List.rev errs)
+    | t :: ts -> (
+        match t.Token.kind with
+        | Token.Err msg -> loop valid (Parser.{ pos = t.pos; msg } :: errs) ts
+        | Token.ErrInvalidChar c ->
+            loop valid
+              (Parser.
+                 {
+                   pos = t.pos;
+                   msg = Format.sprintf "Invalid character '%c'" c;
+                 }
+              :: errs)
+              ts
+        | _ -> loop (t :: valid) errs ts)
+  in
+  loop [] [] tokens
 
 let rec get_tokens lexer =
   let rec collect lex acc =
     let tok, next_lex = Lexer.next lex in
-    if Token.is_error tok then begin
-      there_were_errors := true
-    end;
-    if tok.kind = Token.EOF then List.rev (tok :: acc)
-    else collect next_lex (tok :: acc)
+    let acc = tok :: acc in
+    if tok.kind = Token.EOF then List.rev acc else collect next_lex acc
   in
   collect lexer []
 
 let dump_tokens_json tokens =
-  let json = `List (List.map Token.to_yojson tokens) in
-  Yojson.Basic.pretty_to_string json
+  `List (List.map Token.to_yojson tokens) |> Yojson.Basic.pretty_to_string
 
 let dump_to_file filename data =
   Out_channel.with_open_text filename (fun oc ->
       Out_channel.output_string oc data)
 
 let usage_msg =
-  "usage: sysploc [-t <dump tokens file> | -a <dump ast file> -v] <source file>"
+  "usage: sysploc [-t <dump tokens file> | -a <dump ast file> | -v] <source \
+   file>"
 
 let dump_tokens_file = ref ""
 let dump_ast_file = ref ""
@@ -79,8 +78,15 @@ let () =
   if !dump_tokens_file <> "" then begin
     dump_to_file !dump_tokens_file (dump_tokens_json all_tokens)
   end;
-  let lex_errors = extract_lex_errors all_tokens in
-  let valid_tokens = extract_lex_nonerrors all_tokens in
+  let valid_tokens, lex_errors = separate_tokens all_tokens in
+
+  if !dump_ast_file = "" then begin
+    if lex_errors <> [] then begin
+      List.iter print_error lex_errors;
+      exit 1
+    end;
+    exit 0
+  end;
 
   let ast_opt, rem_tok, parse_errs =
     match Parser.unwrap Parser.parse_program valid_tokens with
@@ -88,11 +94,12 @@ let () =
     | Error errs -> (None, [], errs)
   in
   let all_errs = lex_errors @ parse_errs in
-  if !dump_ast_file <> "" || !verbose then begin
+  if !verbose then begin
+    print_endline "parsing:";
     (match ast_opt with
     | Some ast ->
-        if all_errs = [] then print_endline "parsed fine"
-        else print_endline "parsed with errors";
+        print_endline
+          (if all_errs = [] then "parsed fine" else "parsed with errors");
         Ast.print_program ast
     | None -> print_endline "ultimate parser fail");
     if rem_tok <> [] then begin
@@ -100,8 +107,7 @@ let () =
       List.iter (fun tok -> print_endline (Token.show tok)) rem_tok
     end
   end;
-  if all_errs <> [] && !dump_ast_file <> "" then begin
+  if all_errs <> [] then begin
     List.iter print_error all_errs;
     exit 1
-  end;
-  if lex_errors <> [] then exit 1 else exit 0
+  end
